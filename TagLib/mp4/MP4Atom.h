0508c34b3650648d7406fcaa3fdd5be58e23ae5f
Scott Perry <dev@numist.net>
2011-08-07 19:49:56 -0500

    Move TLTag to a sane location, new skeleton MP4Atom.
    Also added a gitignore for DS_Store. Getting used to this git, Xcode,
    Obj-C, every thing.

---
+++ TagLib/mp4/MP4Atom.h
@@ -0,0 +1,13 @@
+//
+//  MP4Atom.h
+//  TagLib
+//
+//  Created by Scott Perry on 8/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MP4Atom : NSObject
+
+@end
---
+++ TagLib/mp4/MP4Atom.m
@@ -0,0 +1,23 @@
+//
+//  MP4Atom.m
+//  TagLib
+//
+//  Created by Scott Perry on 8/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "MP4Atom.h"
+
+@implementation MP4Atom
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
