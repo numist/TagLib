14c031d8f6dbf640871fa11004de2740a68251a9
Scott Perry <dev@numist.net>
2011-08-07 22:07:35 -0500

    Basic implementation of MP4Atom
    Does not yet include file parsing—that's next. Then: unit tests.

--- TagLib/mp4/MP4Atom.h
+++ TagLib/mp4/MP4Atom.h
@@ -3,11 +3,26 @@
 //  TagLib
 //
 //  Created by Scott Perry on 8/7/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Scott Perry. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface MP4Atom : NSObject
+@interface MP4Atom : NSObject {
+    @protected
+        long offset;
+        long length;
+        NSString *selfName;
+        NSMutableDictionary *children;
+
+    @private
+        NSMutableArray *containers;
+}
+
+- (MP4Atom *) initWithFile: (NSFileHandle *)file;
+- (MP4Atom*) find: (NSArray *)path;
+- (BOOL) getAtoms: (NSMutableArray *)atoms withPath: (NSMutableArray *)path;
+- (NSArray *) findAll: (NSString *)name;
+- (NSArray *) findAll: (NSString *)name recursive: (BOOL)recursive;
 
 @end
--- TagLib/mp4/MP4Atom.m
+++ TagLib/mp4/MP4Atom.m
@@ -3,7 +3,7 @@
 //  TagLib
 //
 //  Created by Scott Perry on 8/7/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Scott Perry. All rights reserved.
 //
 
 #import "MP4Atom.h"
@@ -12,12 +12,65 @@
 
 - (id)init
 {
-    self = [super init];
-    if (self) {
-        // Initialization code here.
+    NSAssert(false, @"%@", @"Attempt to create MP4Atom object without a file");
+    return nil;
+}
+
+- (MP4Atom *) initWithFile: (NSFileHandle *)file
+{
+    NSAssert(false, @"%@", @"This function is not yet implemented!");
+    return nil;
+}
+
+- (MP4Atom *) find: (NSMutableArray *)path
+{
+    MP4Atom *child = [children objectForKey:[path objectAtIndex:0]];
+    
+    [path removeObjectAtIndex:0];
+    if ([path count] == 0) {
+        return child;
+    }
+    return [child find:path];
+}
+
+- (BOOL) getAtoms: (NSMutableArray *)atoms withPath: (NSMutableArray *)path
+{
+    MP4Atom *atom = [children objectForKey:[path objectAtIndex:0]];
+    
+    if (!atom) {
+        return false;
     }
     
-    return self;
+    [path removeObjectAtIndex:0];
+    if ([path count] == 0) {
+        return true;
+    }
+    
+    return [atom getAtoms:atoms withPath:path];
+}
+
+- (NSArray *) findAll: (NSString *)name
+{
+    return [self findAll:name recursive:false];
+}
+
+- (NSArray *) findAll: (NSString *)name recursive: (BOOL)recursive
+{
+    NSMutableArray *hits = [[NSMutableArray alloc] init];
+    MP4Atom *hit;
+    
+    if((hit = [children objectForKey:name])) {
+        [hits addObject:hit];
+    }
+    
+    if(recursive) {
+        NSEnumerator *enumerator = [children objectEnumerator];
+        for (MP4Atom *child in enumerator) {
+            [hits addObjectsFromArray:[child findAll:name recursive:recursive]];
+        }
+    }
+    
+    return hits;
 }
 
 @end
