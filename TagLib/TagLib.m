be9548a694b19f31ab0ddb72dcb258f0615efe42
Scott Perry <dev@numist.net>
2011-08-07 22:06:30 -0500

    __MyCompanyName__
    Urf. Fixed in Address Book, hopefully.

--- TagLib/TagLib.m
+++ TagLib/TagLib.m
@@ -3,7 +3,7 @@
 //  TagLib
 //
 //  Created by Scott Perry on 8/7/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//  Copyright 2011 Scott Perry. All rights reserved.
 //
 
 #import "TagLib.h"
