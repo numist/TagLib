d7aab093696a3eda0c402b3729441db256b6ae15
Scott Perry <dev@numist.net>
2011-08-09 12:57:10 -0500

    Initial implementation of MP4Tag (read-only)
    allows you to set tags within the object, but won't write them to a
    file.  also, none of this stuff has ever been run on real data because I
    don't have Xcode 4.2 and therefore don't have ARC, so… you know… nothing
    ever gets freed. and it will probably crash a lot anyway because it's
    never been tested.

--- TagLib/mp4/MP4Tag.h
+++ TagLib/mp4/MP4Tag.h
@@ -3,11 +3,26 @@
 //  TagLib
 //
 //  Created by Scott Perry on 8/8/11.
-//  Copyright 2011 Scott Perry. All rights reserved.
+//  Copyright 2011 Scott Perry.
+//  This file is based on LGPL/MPL code written by Lukáš Lalinský.
 //
 
 #import <Foundation/Foundation.h>
+#import "TLTag.h"
+#import "MP4Atoms.h"
 
-@interface MP4Tag : NSObject
+
+@interface MP4Tag : TLTag {
+    NSMutableDictionary *itemListMap;
+}
+@property(nonatomic, readonly) NSMutableDictionary *itemListMap;
+
+- (MP4Tag *) initWithFile: (NSFileHandle *)file atoms: (MP4Atoms *)atoms;
+- (BOOL) save;
+
+// NOTE:
+// - (NSString *) albumArtist;
+// - (void) setAlbumArtist: (NSString *)albumArtist;
+// etc.
 
 @end
--- TagLib/mp4/MP4Tag.m
+++ TagLib/mp4/MP4Tag.m
@@ -3,21 +3,328 @@
 //  TagLib
 //
 //  Created by Scott Perry on 8/8/11.
-//  Copyright 2011 Scott Perry. All rights reserved.
+//  Copyright 2011 Scott Perry.
+//  This file is based on LGPL/MPL code written by Lukáš Lalinský.
 //
 
 #import "MP4Tag.h"
+#import "ID3v1Genres.h"
+#import "MP4AtomNames.h"
+
+@interface MP4Tag () {
+    NSMutableDictionary *_items;
+    MP4Atoms *_atoms;
+    NSFileHandle *_file;
+}
+
+- (NSArray *) parseDataForAtom: (MP4Atom *)atom;
+- (NSArray *) parseDataForAtom: (MP4Atom *)atom expectedFlags: (int32_t)flags freeForm: (BOOL)freeForm;
+- (void) parseTextForAtom: (MP4Atom *)atom;
+- (void) parseTextForAtom: (MP4Atom *)atom expectedFlags: (int32_t)flags;
+- (void) parseFreeFormForAtom: (MP4Atom *)atom;
+- (void) parseIntForAtom: (MP4Atom *)atom;
+- (void) parseGnreForAtom: (MP4Atom *)atom;
+- (void) parseIntPairForAtom: (MP4Atom *)atom;
+- (void) parseBoolForAtom: (MP4Atom *)atom;
+- (void) parseCovrForAtom: (MP4Atom *)atom;
+
+@end
+
 
 @implementation MP4Tag
 
-- (id)init
+- (NSDictionary *) itemListMap
+{
+    return self->itemListMap;
+}
+
+- (MP4Tag *) initWithFile: (NSFileHandle *)file atoms: (MP4Atoms *)atoms
 {
     self = [super init];
-    if (self) {
-        // Initialization code here.
+    if (!self) {
+        return nil;
     }
     
+    self->_items = [[NSMutableDictionary alloc] init];
+    self->_atoms = atoms;
+    self->_file = file;
+
+    MP4Atom *ilst = [atoms findAtomAtPath:[NSArray arrayWithObjects:@"moov", @"udta", @"meta", @"ilst", nil]];
+    if (!ilst) {
+        TLLog(@"%@", "Atom moov.udta.meta.ilst not found.");
+        return nil;
+    }
+    
+    for (MP4Atom *atom in [ilst children]) {
+        [self->_file seekToFileOffset:[atom offset]];
+        if ([[atom name] isEqualToString:@"----"]) {
+            [self parseFreeFormForAtom:atom];
+        } else if ([[atom name] isEqualToString:kTrackNumber] ||
+                   [[atom name] isEqualToString:kDiskNumber]) {
+            [self parseIntPairForAtom:atom];
+        } else if ([[atom name] isEqualToString:kCompilation] ||
+                   [[atom name] isEqualToString:kGaplessPlayback] ||
+                   [[atom name] isEqualToString:kPodcast]) {
+            [self parseBoolForAtom:atom];
+        } else if ([[atom name] isEqualToString:kBPM]) {
+            [self parseIntForAtom:atom];
+        } else if ([[atom name] isEqualToString:kGenreCode]) {
+            [self parseGnreForAtom:atom];
+        } else if ([[atom name] isEqualToString:kArtwork]) {
+            [self parseCovrForAtom:atom];
+        } else {
+            [self parseTextForAtom:atom];
+        }
+    }
+
     return self;
+}
+
+- (BOOL) save
+{
+    return NO;
+}
+
+- (NSString *) title
+{
+    return [self->_items objectForKey:kTitle];
+}
+
+- (NSString *) artist
+{
+    return [self->_items objectForKey:kArtist];
+}
+
+- (NSString *) album
+{
+    return [self->_items objectForKey:kAlbum];
+}
+
+- (NSString *) comment
+{
+    return [self->_items objectForKey:kComment];
+}
+
+- (NSString *) genre
+{
+    return [self->_items objectForKey:kGenre];
+}
+
+- (NSNumber *) year
+{
+    long result = [[self->_items objectForKey:kYear] integerValue];
+    return result ? [NSNumber numberWithLong:result] : nil;
+}
+
+- (NSNumber *) track
+{
+    return [[self->_items objectForKey:kTrackNumber] objectAtIndex:0];
+}
+
+- (void) setTitle: (NSString *) title
+{
+    [self->_items setObject:title forKey:kTitle];
+}
+
+- (void) setArtist: (NSString *) artist
+{
+    [self->_items setObject:artist forKey:kArtist];
+}
+
+- (void) setAlbum: (NSString *) album
+{
+    [self->_items setObject:album forKey:kAlbum];
+}
+
+- (void) setComment: (NSString *) comment
+{
+    [self->_items setObject:comment forKey:kComment];
+}
+
+- (void) setGenre: (NSString *) genre
+{
+    [self->_items setObject:genre forKey:kGenre];
+}
+
+- (void) setYear: (NSNumber *) year
+{
+    if (![year isEqualToNumber:[NSNumber numberWithLong:0]]) {
+        [self->_items setObject:year forKey:kYear];
+    } else {
+        [self->_items setObject:@"" forKey:kYear];
+    }
+}
+
+- (void) setTrack: (NSNumber *) track
+{
+    [self->_items setObject:[NSArray arrayWithObjects:track, [NSNumber numberWithInt:0], nil]
+                  forKey:kTrackNumber];
+}
+
+
+- (NSArray *) parseDataForAtom: (MP4Atom *)atom
+{
+    return [self parseDataForAtom:atom expectedFlags:-1 freeForm:NO];
+}
+
+- (NSArray *) parseDataForAtom: (MP4Atom *)atom expectedFlags: (int32_t)expectedFlags freeForm: (BOOL)freeForm
+{
+    NSMutableArray *result = [[NSMutableArray alloc] init];
+    NSData *data = [self->_file readDataOfLength:([atom length] - 8)];
+    int i = 0;
+    unsigned int pos = 0;
+    while (pos < [data length]) {
+        unsigned int length;
+        [data getBytes:&length range:NSMakeRange(pos, 4)];
+        
+        NSString *name = [[NSString alloc] initWithBytes:[[data subdataWithRange:NSMakeRange(pos + 4, 4)] bytes]
+                                                  length:4
+                                                encoding:NSMacOSRomanStringEncoding];
+
+        unsigned int flags;
+        [data getBytes:&flags range:NSMakeRange(pos + 8, 4)];
+        
+        if (freeForm && i < 2) {
+            if (i == 0 && ![name isEqualToString:@"mean"]) {
+                TLLog(@"MP4: Unexpected atom \"%@\", expecting \"mean\"", name);
+                return nil;
+            } else if (i == 1 && ![name isEqualToString:@"name"]) {
+                TLLog(@"MP4: Unexpected atom \"%@\", expecting \"name\"", name);
+                return nil;
+            }
+            [result addObject:[data subdataWithRange:NSMakeRange(pos + 12, length - 12)]];
+        } else {
+            if (![name isEqualToString:@"data"]) {
+                TLLog(@"MP4: Unexpected atom \"%@\", expecting \"data\"", name);
+                return nil;
+            }
+            if (expectedFlags == -1 || flags == expectedFlags) {
+                [result addObject:[data subdataWithRange:NSMakeRange(pos + 16, length - 16)]];
+            }
+        }
+        
+        pos += length;
+        ++i;
+    }
+    
+    return result;
+}
+
+- (void) parseTextForAtom: (MP4Atom *)atom
+{
+    [self parseTextForAtom:atom expectedFlags:-1];
+}
+
+- (void) parseTextForAtom: (MP4Atom *)atom expectedFlags: (int32_t)flags
+{
+    NSArray *data = [self parseDataForAtom:atom];
+    TLCheck([data count] == 1);
+    NSData *datum = [data objectAtIndex:0];
+    TLLog(@"datum size: %u", [datum length]);
+    
+    if ([datum length]) {
+        [self->_items setValue:[NSString stringWithCString:[datum bytes] encoding:NSUTF8StringEncoding]
+                        forKey:[atom name]];
+    }
+}
+
+- (void) parseFreeFormForAtom: (MP4Atom *)atom
+{
+    NSArray *data = [self parseDataForAtom:atom expectedFlags:1 freeForm:YES];
+    TLCheck([data count] > 2);
+    
+    NSString *name = [NSString stringWithFormat:@"----:%@:%@",
+                      [NSString stringWithCString:[[data objectAtIndex:0] bytes]
+                                         encoding:NSMacOSRomanStringEncoding],
+                      [NSString stringWithCString:[[data objectAtIndex:1] bytes]
+                                         encoding:NSMacOSRomanStringEncoding]];
+    
+    NSMutableString *value = [[NSMutableString alloc] init];
+    for (unsigned int i = 2; i < [data count]; ++i) {
+        [value appendString:[NSString stringWithCString:[[data objectAtIndex:i] bytes]
+                                               encoding:NSUTF8StringEncoding]];
+    }
+    
+    [self->_items setValue:value forKey:name];
+}
+
+- (void) parseIntForAtom: (MP4Atom *)atom
+{
+    NSArray *data = [self parseDataForAtom:atom];
+    TLCheck([data count] == 1);
+    NSData *datum = [data objectAtIndex:0];
+    TLLog(@"datum size: %u", [datum length]);
+    
+    if ([datum length]) {
+        unsigned int value;
+        [datum getBytes:&value];
+        if (value > UINT16_MAX) {
+            TLLog(@"Parsed value (%u) that won't fit in 16 bits", value);
+        }
+        [self->_items setValue:[NSNumber numberWithUnsignedShort:value] forKey:[atom name]];
+    }
+}
+
+- (void) parseGnreForAtom: (MP4Atom *)atom
+{
+    NSArray *data = [self parseDataForAtom:atom];
+    TLCheck([data count] == 1);
+    NSData *datum = [data objectAtIndex:0];
+    TLLog(@"datum size: %u", [datum length]);
+    
+    if ([datum length]) {
+        unsigned int index;
+        [datum getBytes:&index];
+        if (index != 0) {
+            NSString *genre = [ID3v1Genres genreForIndex:(index - 1)];
+            if (genre) {
+                [self->_items setValue:genre forKey:[atom name]];
+            }
+        }
+    }
+}
+
+- (void) parseIntPairForAtom: (MP4Atom *)atom
+{
+    NSArray *data = [self parseDataForAtom:atom];
+    TLCheck([data count] == 1);
+    NSData *datum = [data objectAtIndex:0];
+    TLLog(@"datum size: %u, parsing bytes in range (2, 4)", [datum length]);
+    
+    if ([datum length]) {
+        unsigned short a, b;
+        [datum getBytes:&a range:NSMakeRange(2, 2)];
+        [datum getBytes:&b range:NSMakeRange(4, 2)];
+        [self->_items setValue:[NSArray arrayWithObjects:[NSNumber numberWithUnsignedShort:a], [NSNumber numberWithUnsignedShort:b], nil]
+                        forKey:[atom name]];
+    }
+}
+
+- (void) parseBoolForAtom: (MP4Atom *)atom
+{
+    NSArray *data = [self parseDataForAtom:atom];
+    TLCheck([data count] == 1);
+    NSData *datum = [data objectAtIndex:0];
+    TLLog(@"datum size: %u", [datum length]);
+    
+    if ([datum length]) {
+        NSNumber *result = nil;
+        if ([datum length]) {
+            uint8 value;
+            [datum getBytes:&value length:1];
+            if (value == 1) {
+                result = [NSNumber numberWithBool:YES];
+            }
+        }
+        if (result == nil) {
+            result = [NSNumber numberWithBool:NO];
+        }
+        [self->_items setValue:result forKey:[atom name]];
+    }
+}
+
+- (void) parseCovrForAtom: (MP4Atom *)atom
+{
 }
 
 @end
