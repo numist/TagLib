457fe3a294efe7ec37403ae2abc0cdb14fdc3867
Scott Perry <dev@numist.net>
2011-08-09 12:53:55 -0500

    collect mp4 atom name constants in one place
    because magic strings bug the crap out of me

--- TagLib/mp4/MP4AtomNames.h
+++ TagLib/mp4/MP4AtomNames.h
@@ -3,11 +3,40 @@
 //  TagLib
 //
 //  Created by Scott Perry on 8/9/11.
-//  Copyright 2011 Scott Perry. All rights reserved.
+//  Copyright 2011 Scott Perry.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface MP4AtomNames : NSObject
-
-@end
+extern NSString *kAlbum;
+extern NSString *kArtist;
+extern NSString *kAlbumArtist;
+extern NSString *kComment;
+extern NSString *kYear;
+extern NSString *kTitle;
+extern NSString *kGenre;
+extern NSString *kGenreCode;
+extern NSString *kTrackNumber;
+extern NSString *kDiskNumber;
+extern NSString *kComposer;
+extern NSString *kEncoder;
+extern NSString *kBPM;
+extern NSString *kCopyright;
+extern NSString *kCompilation;
+extern NSString *kArtwork;
+extern NSString *kRating;
+extern NSString *kGrouping;
+extern NSString *kPodcast;
+extern NSString *kCategory;
+extern NSString *kKeyword;
+extern NSString *kPodcastURL;
+extern NSString *kEpisodeGUID;
+extern NSString *kDescription;
+extern NSString *kLyrics;
+extern NSString *kTVNetworkName;
+extern NSString *kTVShowName;
+extern NSString *kTVEpisodeNumber;
+extern NSString *kTVSeason;
+extern NSString *kTVEpisode;
+extern NSString *kPurchaseDate;
+extern NSString *kGaplessPlayback;
--- TagLib/mp4/MP4AtomNames.m
+++ TagLib/mp4/MP4AtomNames.m
@@ -3,21 +3,41 @@
 //  TagLib
 //
 //  Created by Scott Perry on 8/9/11.
-//  Copyright 2011 Scott Perry. All rights reserved.
+//  Copyright 2011 Scott Perry.
+//  Tag information from: http://atomicparsley.sourceforge.net/mpeg-4files.html
 //
 
 #import "MP4AtomNames.h"
 
-@implementation MP4AtomNames
-
-- (id)init
-{
-    self = [super init];
-    if (self) {
-        // Initialization code here.
-    }
-    
-    return self;
-}
-
-@end
+kAlbum = @"©alb";           // flags: 1 type: text
+kArtist = @"©art";          // flags: 1 type: text
+kAlbumArtist = @"aART";     // flags: 1 type: text
+kComment = @"©cmt";         // flags: 1 type: text
+kYear = @"©day";            // flags: 1 type: text
+kTitle = @"©nam";           // flags: 1 type: text
+kGenre = @"©gen";           // flags: 1 type: text
+kGenreCode = @"gnre";       // flags: 0 type: uint8
+kTrackNumber = @"trkn";     // flags: 0 type: uint8
+kDiskNumber = @"disk";      // flags: 0 type: uint8
+kComposer = @"©wrt";        // flags: 1 type: text
+kEncoder = @"©too";         // flags: 1 type: text
+kBPM = @"tmpo";             // flags: 21 type: uint8
+kCopyright = @"cprt";       // flags: 1 type: text
+kCompilation = @"cpil";     // flags: 21 type: uint8
+kArtwork = @"covr";         // flags: ≥ 13 type: jpeg | png
+kRating = @"rtng";          // flags: 21 type: uint8
+kGrouping = @"©grp";        // flags: 1 type: text
+kPodcast = @"pcst";         // flags: 21 type: uint8
+kCategory = @"catg";        // flags: 1 type: text
+kKeyword = @"keyw";         // flags: 1 type: text
+kPodcastURL = @"purl";      // flags: 21 | 0 (Apple changed from 21 to the current 0 around iTunes 6.0.3) type: uint8
+kEpisodeGUID = @"egid";     // flags: 21 | 0 (Apple changed from 21 to the current 0 around iTunes 6.0.3) type: uint8
+kDescription = @"desc";     // flags: 1 type: text
+kLyrics = @"©lyr";          // flags: 1 type: text (Lyrics is the only text atom that doesnt't fall under a 255byte limit)
+kTVNetworkName = @"tvnn";   // flags: 1 type: text
+kTVShowName = @"tvsh";      // flags: 1 type: text
+kTVEpisodeNumber = @"tven"; // flags: 1 type: text
+kTVSeason = @"tvsn";        // flags: 21 type: uint8
+kTVEpisode = @"tves";       // flags: 21 type: uint8
+kPurchaseDate = @"purd";    // flags: 1 type: text
+kGaplessPlayback = @"pgap"; // flags: 21 type: uint8
